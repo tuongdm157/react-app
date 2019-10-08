0cc2334eceffd15c5b38e3a0802034dda007e7ff
dmtuong <dmtuong@cmc.com.vn>
2019-10-08 22:42:56 +0700

    lecture27

--- src/App.js
+++ src/App.js
@@ -1,87 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import ToDoItems from './components/ToDoItems';
-import Counter from './components/Counter';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import TrafficLight from './componentsState/TrafficLight';
-import Accordion from './componentChild/Accordion';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showCounter: true
-    }
-    // this.inputElement = React.createRef();
-
-    // this.state = {
-    //   toDoItem: [
-    //     { title: "Go to market", isComplete: true },
-    //     { title: "Play fooball", isComplete: true },
-    //     { title: "Go to buy snack" }
-    //   ]
-    // }
-  }
-
-  // componentDidMount(){
-  //   this.inputElement.current.focus();
-  // }
-
-  // onItemClick(item) {
-  //   return (event) => {
-  //     const isComplete = item.isComplete;
-  //     const { toDoItem } = this.state
-  //     const index = toDoItem.indexOf(item);
-  //     this.setState({
-  //       toDoItem: [
-  //         ...toDoItem.slice(0, index),
-  //         { ...item, isComplete: !isComplete },
-  //         ...toDoItem.slice(index + 1)
-  //       ]
-  //     })
-  //   }
-  // }
-
-  removeCounter = () =>{
-    this.setState({
-      showCounter: false
-    })
-  }
-
-  render() {
-    // const { toDoItem } = this.state
-    return (
-      <div className="App">
-        <button onClick={this.removeCounter}>RemoveCounter</button>
-        {this.state.showCounter && <Counter></Counter>}
-        {/* <input type="text" ref={this.inputElement}></input> */}
-        {/* <Accordion heading="Heading" >
-          Demo ne
-        </Accordion> */}
-        {/* <TrafficLight/> */}
-        {/* {
-          toDoItem.length > 0 && toDoItem.map((item, index) => <ToDoItems
-            key={index}
-            item={item}
-            onClick={this.onItemClick(item)}></ToDoItems>)
-        }
-        {toDoItem.length === 0 && 'Nothing....'} */}
-
-
-      </div>
-    );
-  }
-
-  componentDidMount(){
-
-  }
-
-  componentDidUpdate(){
-
-  }
-
-  componentWillUnmount(){
-
-  }
+import TopMenu from "./components/TopMenu";
+function Home() {
+  return <h2>Home</h2>;
 }
 
-export default App;
+function About() {
+  return <h2>Product</h2>;
+}
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <TopMenu/>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
